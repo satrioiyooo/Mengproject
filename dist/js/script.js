2b2737f59d9f51f2fde417fb0b6929a1163cf412
satrioiyooo <satriowibowo004@gmail.com>
2022-08-28 00:01:00 +0700

    Mengproject

---
+++ dist/js/script.js
@@ -0,0 +1,30 @@
+//hamburger
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
+
+//navbar fixed
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixednav = header.offsetTop;
+
+  if (window.pageYOffset > fixednav) {
+    header.classList.add("navbar-fixed");
+  } else {
+    header.classList.remove("navbar-fixed");
+  }
+};
+
+//scrolling navbar
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
